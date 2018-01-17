b79073a7c379366e5e46cf0f2d7779ff0d562af0
Iarlen <iarlensilva775@gmail.com>
2018-01-17 15:00:56 -0300

    Criando o componente

---
+++ src/components/OutrosJogos.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text
+} from 'react-native';
+
+export default class OutroJogos extends Component {
+	render() {
+		return (
+			<Text style={{ flex: 1, backgroundColor: '#61BD8C' }}>
+			  Aqui podem ser apresentadas informações sobre outos jogos do desenvolvedor.
+			</Text>
+		);
+	}
+}
