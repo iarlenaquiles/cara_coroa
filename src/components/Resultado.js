f5aa02279bf70a08bb6363e5a11b783c2ca574f0
Iarlen <iarlensilva775@gmail.com>
2018-01-17 15:29:48 -0300

    Mostrando as imagens das moedas

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text
+  Image,
+  View
 } from 'react-native';
+
+const cara = require('../imgs/moeda_cara.png');
+const coroa = require('../imgs/moeda_coroa.png');
 
 export default class Resultado extends Component {
 	render() {
 		return (
-			
+			<View>
+				<Image source={cara}/>
+				<Image source={coroa}/>
+			</View>
 		);
 	}
 }
