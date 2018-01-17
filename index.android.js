841bc208938aec0f6132e1bad7e60829136e0d62
Iarlen <iarlensilva775@gmail.com>
2018-01-17 15:12:03 -0300

    Criando as rotas

--- index.android.js
+++ index.android.js
@@ -4,10 +4,18 @@ import {
 } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 
+import Principal from './src/components/Principal';
+import SobreJogo from './src/components/SobreJogo';
+import OutrosJogos from './src/components/OutrosJogos';
+
 export default class cara_coroa extends Component {
   render() {
     return (
-      
+      <Router>
+        <Scene key='principal' component={Principal} initil title="Cara ou Coroa" />
+        <Scene key='sobrejogo' component={SobreJogo} />
+        <Scene key='outrosjogos'  component={OutrosJogos} />
+      </Router>
     );
   }
 }
