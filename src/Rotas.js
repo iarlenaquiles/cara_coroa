f0cbb7139f55bb2894027bc943bcdb399ceadd54
Iarlen <iarlensilva775@gmail.com>
2018-01-17 15:51:22 -0300

    Isolando as rotas da aplicação

---
+++ src/Rotas.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Router, Scene } from 'react-native-router-flux';
+
+import Principal from './components/Principal';
+import SobreJogo from './components/SobreJogo';
+import OutrosJogos from './components/OutrosJogos';
+import Resultado from './components/Resultado';
+
+const Rotas = () => (
+	 <Router sceneStyle={{ paddingTop: 50}}>
+        <Scene key='principal' component={Principal} initil title="Cara ou Coroa" />
+        <Scene key='sobrejogo' component={SobreJogo} title="Sobre o Jogo" />
+        <Scene key='outrosjogos'  component={OutrosJogos} title="Outro Jogos" />
+        <Scene key='resultado'  component={Resultado} title="Resultado" />
+      </Router>
+);
+
+
+export default Rotas;
